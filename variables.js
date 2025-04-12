f6e993ac8aa0caefc4e67b9d1da7e5f5a4fecfb7
wajahat taj <p233023@pwr.nu.edu.pk>
2025-04-12 21:37:14 +0500

    chnages

---
+++ variables.js
@@ -0,0 +1,20 @@
+const number = 10923; // same as cpp constant through the whole code 
+
+let name = "saeed_taj"; // let is more preferable 
+
+var email = " saedtaj00@gmail.com"; // prefered to not use the var because of block scope and functional scope 
+
+myname = "wajahattaj";  //js allows this but we shouldn't do that abviously  
+
+name = " p233023@pwr.nu.edu.pk";
+email = "myemail@gmail.com";
+name = 32.8923;  // we can store integers , strings and floats with using one datatype keyword let
+
+console.log(number);
+console.log(name);
+console.log(email);
+console.log(myname);
+console.log(myname);
+
+console.table([number , name , email , myname]);  // printing in the table form with indexes and the values 
+
