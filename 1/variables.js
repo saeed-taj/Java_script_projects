9fd2bdca9700d74999eefb37c6f4706c9386caf5
wajahat taj <p233023@pwr.nu.edu.pk>
2025-04-14 00:21:18 +0500

    commit_1

---
+++ 1/variables.js
@@ -0,0 +1,11 @@
+
+console.log("wajahat Taj");
+
+const account_Id = "wajahat@gmail.com";
+var account_Password = "1234567890";
+account_city = "Karachi";
+
+
+console.log();
+console.table([account_Id, account_Password, account_city]);
+
