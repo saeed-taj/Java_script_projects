c2b87726e94889edec623bed1852fecd5a7a2716
wajahat taj <p233023@pwr.nu.edu.pk>
2025-04-24 17:45:12 +0500

    day2

--- 1/variables.js
+++ 1/variables.js
@@ -9,3 +9,8 @@ account_city = "Karachi";
 console.log();
 console.table([account_Id, account_Password, account_city]);
 
+/*
+prefer not to use the var keyword for declaring variables, 
+as it has function scope and can lead to unexpected behavior. 
+Instead, use let or const for block-scoped variables.
+*/
---
+++ 2/data_types.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all code as newer version of javascript
+
+//alert (3 + 3); // we are using nodejs not browser
+
+console.log(3 + 3); 
+
+console.log("wajahat Taj");
+
+let name = "wajahat Taj";
+let age = 22;
+let isstudent = true;
+
+// number => 2 to the power of 53
+// BigInt => 2 to the power of 53 + 1
+// string => "wajahat Taj"
+// boolean => true or false
+// null => empty value 
+// undefined => variable is declared but not assigned any value
+// symbol => unique value
+
+console.log(typeof "name");
+console.log(typeof age);
+console.log(typeof undefined ); // output : undefined
+console.log(typeof null); // output : object
+
