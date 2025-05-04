19391f3a9ade7e26911876ee03978d77ae35266a
wajahat taj <p233023@pwr.nu.edu.pk>
2025-05-04 23:28:00 +0500

    commit

---
+++ 2/conversion_operation.js
@@ -0,0 +1,11 @@
+"use strict"
+
+
+console.log("1" + 2); //12
+console.log(1 + "2");  //12
+console.log("1" + 2 + 2); //122 
+console.log(1 + 2 + "2"); //32  // not good practice
+
+console.log(+true); // 1
+console.log(true + 1); // 2
+console.log(true + "1");
