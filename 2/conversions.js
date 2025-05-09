6d7ae1da29dc641dff6c8e4e623883c641344920
wajahat taj <p233023@pwr.nu.edu.pk>
2025-05-10 01:19:40 +0500

    6th

---
+++ 2/conversions.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let isLoggedIn = 1;
+
+let BooleanIsLoggedIn = Boolean(isLoggedIn);   // Convert to boolean
+console.log(BooleanIsLoggedIn);
+
+
+let somenumber = 34;
+
+let string_Number = string(somenumber);
+console.log(string_Number);
+console.log(typeof string_Number);
+
