3ef6ad3684c1abac11a9c6ade638a603210cd1ad
wajahat taj <p233023@pwr.nu.edu.pk>
2025-05-11 02:15:16 +0500

    8th

---
+++ 2/more_objects.js
@@ -0,0 +1,23 @@
+"use strict"
+
+
+
+const regular_User = {
+
+
+    email : "wajahattaj@gmail.com",
+    
+    Full_name : {
+       
+        User_full_name : {
+        
+        first_name : " wajahat",
+        Middle_name : " ",
+        Last_name : "Taj"
+    }
+}
+}
+
+console.log(regular_User.Full_name);
+console.log(regular_User.Full_name.User_full_name.first_name);
+console.log(regular_User.Full_name.User_full_name);
