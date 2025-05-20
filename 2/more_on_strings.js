8a61a3106f0bfb11969807dab63f59069f74a146
wajahat taj <p233023@pwr.nu.edu.pk>
2025-05-21 00:44:28 +0500

    10th

---
+++ 2/more_on_strings.js
@@ -0,0 +1,20 @@
+"use strict"
+
+
+let name = new String("saeed");
+
+
+console.log(name[0]);//s
+console.log(Object.getPrototypeOf(name));
+
+console.log(name.length);
+
+console.log(name[1]); // Example: Accessing the second character
+
+console.log(name.toUpperCase());
+
+console.log(name.charAt(4));
+
+console.log(name.charAt(0)); // Example: Accessing the first character
+
+console.log(name.indexOf('e'));
