6b6c5df2c4d6dc71621f93491c9cdc35bb96280e
wajahat taj <p233023@pwr.nu.edu.pk>
2025-06-17 22:30:45 +0500

    objects_prt3

---
+++ 2/objects_prt3.js
@@ -0,0 +1,15 @@
+"use strict"
+
+
+const courses = {
+
+    name : "Data structures and algorithms",
+    id : 3023,
+    email : "saeedtaj00@gmail.com"
+
+}
+
+
+const {name : na} = courses;
+
+console.log(na); 
