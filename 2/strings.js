72b5f34799b6a0384abecd717ed3eb5414d152d5
wajahat taj <p233023@pwr.nu.edu.pk>
2025-06-27 00:55:39 +0500

    strings

---
+++ 2/strings.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let name = "saeed";
+
+let repo_count = 10;
+
+
+// string interpolation
+console.log(`my name is ${name} and my repo count is ${repo_count} `); 
+
+
+
+// string concatenation
+console.log("my name is "+ name +" and my repo count is "+ repo_count+"");
