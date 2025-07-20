4c57368b21a299ddfd2b1798c3ea2df5317790a8
wajahat taj <p233023@pwr.nu.edu.pk>
2025-07-20 20:59:28 +0500

    maps

--- Functions/maps.js
+++ Functions/maps.js
@@ -1,7 +1,8 @@
           "use strict"
 
 
-const map = new Map();
+const map = new Map();  // map using object means new makes new object 
+
 map.set('PK' , "Pakistan");
 map.set('US' , "United states of america");
 map.set('FR' , "France");
@@ -14,3 +15,11 @@ for (const [key , value] of map){
     console.log(key , ':-' , value);
 }
 
+
+
+// example
+const numbers = [1, 2, 3, 4];
+
+const doubled = numbers.map((num) => num * 2);
+
+console.log(doubled);
